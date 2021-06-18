c2d45d11a570d83403cd3baef49957d5d66999b3
Johan-Liebert1 <pragyanpoudyal41999@gmail.com>
2021-06-18 19:24:34 +0530

    initial setup

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <h1>{{ name }} - {{ age }}</h1>
+  <button @click="changeName('Tenma')">ChangeName</button>
+  <button @click="changeAge('36')">Age</button>
+</template>
+
+<script lang="ts">
+import { defineComponent, reactive, toRefs } from "vue";
+
+export default defineComponent({
+  name: "App",
+  components: {},
+  setup() {
+    const state = reactive({
+      name: "Johan",
+      age: 25 as number | string
+    });
+
+    return { ...toRefs(state) };
+  },
+  methods: {
+    changeName(newName: string) {
+      this.name = newName;
+    },
+
+    changeAge(newAge: number | string) {
+      this.age = newAge;
+    }
+  }
+});
+</script>
+
+<style></style>
